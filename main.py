1511ff2475aef9fa2b13ea394df7d414997a3dfd
Ankur Singh <nk.ankur1711@gmail.com>
2019-06-03 23:06:19 +0530

    Solved the lazy running issue and now onto callbacks

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from transform import AvgRating
+from transform import df_count_by_category
+import plotly.graph_objs as go
 import pandas as pd
-
-
-df = pd.read_csv("Google-Playstore-32K.csv")
-columns = list(df.columns)
-
-df['Rating_validated'] = pd.to_numeric(df['Rating'], errors='coerce')
-
-df_count_by_category = df.groupby('Category').mean().dropna(axis=0).sort_values('Rating_validated',ascending=False).head()
+from dash.dependencies import Input,Output
 
 app = dash.Dash()
 
-print(list(df_count_by_category.index))
+app.layout = html.Div([dcc.Graph(id='graph')])
 
-app.layout = html.Div([
-                    html.Div("Average Rating by Category"),
-                    dcc.Graph(id='AvgRating',
-                                figure=AvgRating(df_count_by_category,list(df_count_by_category.index), df_count_by_category['Rating_validated'], "Avg Rating"))
-])
+@app.callback(Output('graph','figure'), Input)
+def AvgRating():
+    trace = [go.Bar(x=list(df_count_by_category.index), y=list(df_count_by_category['Rating_validated']))]
+    print(go)
+    print(trace)
+    return {'data': trace,
+                'layout': go.Layout(title='Hope this works') }
+
+
 
 
 
--- transform.py
+++ transform.py
@@ -9,7 +9,9 @@ df['Rating_validated'] = pd.to_numeric(df['Rating'], errors='coerce')
 df_count_by_category = df.groupby('Category').mean().dropna(axis=0).sort_values('Rating_validated',ascending=False).head()
 
 def AvgRating (dataframe,x,y,title):
-    x_value = x
-    y_value = y
+    return dict(data=[go.Bar(x=x, y=y)], layout=go.Layout(title=title))
+    print(x)
+    print(y)
 
-    return dict(data=[go.Bar(x=x_value, y=y_value)], layout=go.Layout(title=title))
+    return {'data': [go.Bar(x=x,y=y)],
+                'layout': go.Layout(title='Hope this works') }
